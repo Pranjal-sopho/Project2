93d1a4c6b34369b31826f48129af8d4f375e993a
Sanjay Khadda <sanjay.khadda12@gmail.com>
2017-02-18 10:05:52 +0000

    done with names of colleges, fees, courses

--- project1/public/scraper.php
+++ project1/public/scraper.php
@@ -4,8 +4,19 @@
         }
         else{
             $data = file_get_contents($_GET["city_url"]);
+            //names of colleges
             preg_match_all('/<a class="institute-title-clr".+ title="(.+)"/', $data, $names);
-            for($i=0; $i<35; $i++)
+            for($i=0; $i<30; $i++)
             echo $names[1][$i];
+            
+            //fees of colleges
+            preg_match_all('/INR (\d+,\d+,\d+)/', $data, $fees);
+            for($i=0; $i<30; $i++)
+            echo $fees[1][$i];
+            
+            //courses offered
+            preg_match_all('/<a href="http:\/\/www.shiksha.com\/.+\n.+(.+).+<\/a>/', $data, $course);
+            for($i=1; $i<31; $i++)
+            echo $course[0][$i];
         }
 ?>
