0490151f64a878321b1094333c88985eaaa352a8
Sanjay Khadda <sanjay.khadda12@gmail.com>
2017-03-08 16:11:14 +0000

    removed unneccesary files

--- pset7/views/quote_display.php
+++
@@ -1,8 +0,0 @@
-<p><h4>Here is the quote of <?=$symbol?></h4></p>
-<p><strong><?= "Name:     " . $name . "<br/>"; ?></strong>
-<strong><?= "Symbol:     " . $symbol . "<br/>"; ?></strong>
-<strong><?= "Price:     $" . number_format($price, 2) . "<br/>"; ?></strong>
-</p>
-<ul class="nav nav-tabs nav-justified" align="center">
-        <li><a href="quote.php">Check another Quote</a></li>
-</ul>
