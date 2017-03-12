76a7a768a36e17a3f9be7c9c937eff9f2d641685
Pranjal Sharma <pranjal.sharmaxxx@gmail.com>
2017-03-12 17:08:01 +0000

    and the bugs wont end git add .

--- public/controllers/index.php
+++ public/controllers/index.php
@@ -1,13 +1,10 @@
 <?php
 
     // configuration
-    require("../includes/helpers.php");
+    require("../../includes/helpers.php");
     
     //starting session
     session.start();
-     
-    //configuration
-    require();
     
     // if user reached page via a GET request
     if ($_SERVER["REQUEST_METHOD"] == "GET")
